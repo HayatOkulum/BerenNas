2d5b446ba89c92ab45aaa9a523e25b9512028c9c
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2023-05-18 03:09:09 +0300

    start project
    start project

---
+++ Matematik/BerenNas/eskenarucgen.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BerenNas
+{
+    partial class eskenarucgen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tB_a = new System.Windows.Forms.TextBox();
+            this.btn_hesapla = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sonuc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BerenNas.Properties.Resources.eskenarucgen;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(564, 412);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(582, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Eşkenar Üçgenin Bir Kenarını Giriniz (a)";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(689, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "a =";
+            // 
+            // tB_a
+            // 
+            this.tB_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tB_a.Location = new System.Drawing.Point(731, 85);
+            this.tB_a.Multiline = true;
+            this.tB_a.Name = "tB_a";
+            this.tB_a.Size = new System.Drawing.Size(45, 35);
+            this.tB_a.TabIndex = 3;
+            // 
+            // btn_hesapla
+            // 
+            this.btn_hesapla.Location = new System.Drawing.Point(791, 91);
+            this.btn_hesapla.Name = "btn_hesapla";
+            this.btn_hesapla.Size = new System.Drawing.Size(75, 23);
+            this.btn_hesapla.TabIndex = 4;
+            this.btn_hesapla.Text = "Hesapla";
+            this.btn_hesapla.UseVisualStyleBackColor = true;
+            this.btn_hesapla.Click += new System.EventHandler(this.btn_hesapla_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(612, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Üçgenin Çevresi =";
+            // 
+            // sonuc
+            // 
+            this.sonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sonuc.ForeColor = System.Drawing.Color.Red;
+            this.sonuc.Location = new System.Drawing.Point(813, 173);
+            this.sonuc.Name = "sonuc";
+            this.sonuc.Size = new System.Drawing.Size(103, 32);
+            this.sonuc.TabIndex = 6;
+            this.sonuc.Text = "??";
+            // 
+            // eskenarucgen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 436);
+            this.Controls.Add(this.sonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_hesapla);
+            this.Controls.Add(this.tB_a);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "eskenarucgen";
+            this.Text = "eskenarucgen";
+            this.Load += new System.EventHandler(this.eskenarucgen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tB_a;
+        private System.Windows.Forms.Button btn_hesapla;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label sonuc;
+    }
+}
---
+++ Matematik/BerenNas/eskenarucgen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BerenNas
+{
+    public partial class eskenarucgen : Form
+    {
+        public eskenarucgen()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_hesapla_Click(object sender, EventArgs e)
+        {
+            int a;
+            int cevre;
+            a = Convert.ToInt32(tB_a.Text);
+            cevre = a * 3;
+            sonuc.Text = cevre.ToString();
+        }
+
+        private void eskenarucgen_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
